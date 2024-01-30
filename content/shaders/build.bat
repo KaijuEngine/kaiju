bbee81eb3854e4e051760c5a5f2198cca1d65f7b
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2024-01-29 19:22:25 -0600

    Updating SPV compile scripts

--- content/shaders/build.bat
+++
@@ -1,4 +0,0 @@
-for /f %%a IN ('dir /b') do call glslc %%a -o spv/%%a.spv
-for /f %%a IN ('dir /b') do call glslc %%a -o spv/%%a.oit.spv -DOIT
-
-pause
--- content/shaders/build_debug.bat
+++
@@ -1,4 +0,0 @@
-for /f %%a IN ('dir /b') do call glslc %%a -o spv/%%a.spv -g
-for /f %%a IN ('dir /b') do call glslc %%a -o spv/%%a.oit.spv -g -DOIT
-
-pause
