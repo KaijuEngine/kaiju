cdc14efa817885bff82630a841ff70683d2cb44d
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2024-01-31 18:22:38 -0600

    vec2/3/4i as arrays

--- src/matrix/vec3i.go
+++ src/matrix/vec3i.go
@@ -1,7 +1,7 @@
 package matrix
 
-type Vec3i struct {
-	X int32
-	Y int32
-	Z int32
-}
+type Vec3i [3]int32
+
+func (v Vec3i) X() int32 { return v[0] }
+func (v Vec3i) Y() int32 { return v[1] }
+func (v Vec3i) Z() int32 { return v[2] }
