3b66d145f782b93119b9c8cb4768864d4b24fb8d
Brett Faulds <brfaulds@gmail.com>
2024-02-03 21:14:24 -0800

    Working filter search somewhat

--- src/editor/project/ui/hierarchy/hierachy.go
+++ src/editor/project/ui/hierarchy/hierachy.go
@@ -3,12 +3,16 @@ package hierarchy
 import (
 	"kaiju/engine"
 	"kaiju/klib"
+	"kaiju/ui"
 	"kaiju/uimarkup"
 	"kaiju/uimarkup/markup"
+	"strings"
 )
 
 type Hierarchy struct {
-	doc *markup.Document
+	doc        *markup.Document
+	input      *ui.Input
+	onChangeId engine.EventId
 }
 
 type entityEntry struct {
@@ -35,4 +39,22 @@ func (h *Hierarchy) Create(host *engine.Host) {
 	}
 	html := klib.MustReturn(host.AssetDatabase().ReadText("ui/hierarchy/hierarchy.html"))
 	h.doc = uimarkup.DocumentFromHTMLString(host, html, "", entries, nil)
+	searchInputElement, _ := h.doc.GetElementById("hierarchyInput")
+	entityList, _ := h.doc.GetElementById("entityList")
+	h.input = searchInputElement.UI.(*ui.Input)
+
+	h.input.Data().OnChange.Remove(h.onChangeId)
+	h.onChangeId = h.input.Data().OnChange.Add(func() {
+		activeText := h.input.Text()
+
+		for idx := range entityList.HTML.Children {
+			label := entityList.HTML.Children[idx].Children[0].DocumentElement.UI.(*ui.Label)
+
+			if strings.Contains(label.Text(), activeText) {
+				entityList.HTML.Children[idx].DocumentElement.UI.Entity().Activate()
+			} else {
+				entityList.HTML.Children[idx].DocumentElement.UI.Entity().Deactivate()
+			}
+		}
+	})
 }
