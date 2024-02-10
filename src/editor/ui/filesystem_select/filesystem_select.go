435662177b03a7c6cbf189bf62108fce4c5e3a06
Brent Farris <brentfarris@outlook.com>
2024-02-10 07:13:50 -0600

    Allow selecting specific extensions

--- src/editor/ui/filesystem_select/filesystem_select.go
+++ src/editor/ui/filesystem_select/filesystem_select.go
@@ -9,6 +9,7 @@ import (
 	"kaiju/ui"
 	"os"
 	"path/filepath"
+	"slices"
 )
 
 type FilesystemSelect struct {
@@ -19,91 +20,111 @@ type FilesystemSelect struct {
 	container  *host_container.HostContainer
 	Dir        []fs.DirEntry
 	Path       string
+	Extensions []string
 	funcMap    map[string]func(*document.DocElement)
 }
 
-func New(title string, onSelected func(string)) {
+// Will create a new window that allows the person to select a file or folder
+// on their system. If the extensions are not empty, then only files with those
+// extensions will be selectable. Otherwise all files or folders can be picked
+func New(title string, extensions []string, onSelected func(string)) {
 	if title == "" {
 		title = "File/Folder Select"
 	}
-	fs := FilesystemSelect{
+	s := FilesystemSelect{
 		onSelected: onSelected,
 		funcMap:    make(map[string]func(*document.DocElement)),
+		Extensions: make([]string, 0, len(extensions)),
 	}
-	fs.funcMap["selectEntry"] = fs.selectEntry
-	fs.container = host_container.New(title)
-	go fs.container.Run(500, 600)
-	<-fs.container.PrepLock
+	for _, ext := range extensions {
+		if ext[0] != '.' {
+			ext = "." + ext
+		}
+		s.Extensions = append(s.Extensions, ext)
+	}
+	s.funcMap["selectEntry"] = s.selectEntry
+	s.container = host_container.New(title)
+	go s.container.Run(500, 600)
+	<-s.container.PrepLock
 	if here, err := os.Getwd(); err != nil {
-		fs.Path = "/"
+		s.Path = "/"
 	} else {
-		fs.Path = here
+		s.Path = here
 	}
-	fs.container.RunFunction(func() {
-		fs.reloadUI()
+	s.container.RunFunction(func() {
+		s.reloadUI()
 	})
-	fs.container.Host.OnClose.Add(func() {
-		fs.onSelected("")
+	s.container.Host.OnClose.Add(func() {
+		s.onSelected("")
 	})
 }
 
-func (fs *FilesystemSelect) selectPath() {
-	fs.container.Host.Close()
-	fs.onSelected(fs.Path)
+func (s *FilesystemSelect) selectPath() {
+	s.container.Host.Close()
+	s.onSelected(s.Path)
 }
 
-func (fs *FilesystemSelect) selectEntry(elm *document.DocElement) {
+func (s *FilesystemSelect) selectEntry(elm *document.DocElement) {
 	path := elm.HTML.Attribute("data-path")
-	fs.Path = filepath.Clean(fs.Path + "/" + path)
-	if info, err := os.Stat(fs.Path); err != nil {
-		fs.container.Host.Close()
+	s.Path = filepath.Clean(s.Path + "/" + path)
+	if info, err := os.Stat(s.Path); err != nil {
+		s.container.Host.Close()
 		return
 	} else {
 		if info.IsDir() {
-			fs.reloadUI()
+			s.reloadUI()
 		} else {
-			fs.container.Host.Close()
-			fs.onSelected(fs.Path)
+			s.container.Host.Close()
+			s.onSelected(s.Path)
 		}
 	}
 }
 
-func (fs *FilesystemSelect) reloadUI() {
-	for _, e := range fs.container.Host.Entities() {
+func (s *FilesystemSelect) reloadUI() {
+	for _, e := range s.container.Host.Entities() {
 		e.Destroy()
 	}
-	fs.list()
-	html := klib.MustReturn(fs.container.Host.AssetDatabase().ReadText("ui/editor/filesystem_select.html"))
-	fs.doc = markup.DocumentFromHTMLString(
-		fs.container.Host, html, "", fs, fs.funcMap)
-	if elm, ok := fs.doc.GetElementById("pathInput"); !ok {
+	s.list()
+	html := klib.MustReturn(s.container.Host.AssetDatabase().ReadText("ui/editor/filesystem_select.html"))
+	s.doc = markup.DocumentFromHTMLString(
+		s.container.Host, html, "", s, s.funcMap)
+	if elm, ok := s.doc.GetElementById("pathInput"); !ok {
 		// TODO:  Log the error
-		fs.container.Host.Close()
+		s.container.Host.Close()
 		return
 	} else {
-		fs.input = elm.UI.(*ui.Input)
+		s.input = elm.UI.(*ui.Input)
+		s.input.Data().OnSubmit.Add(s.submit)
 	}
-	if elm, ok := fs.doc.GetElementById("listing"); !ok {
+	if elm, ok := s.doc.GetElementById("listing"); !ok {
 		// TODO:  Log the error
-		fs.container.Host.Close()
+		s.container.Host.Close()
 		return
 	} else {
-		fs.listing = elm.UIPanel
+		s.listing = elm.UIPanel
 	}
 }
 
-func (fs *FilesystemSelect) submit() {
-
+func (s *FilesystemSelect) submit() {
+	s.Path = s.input.Text()
+	s.reloadUI()
 }
 
-func (fs *FilesystemSelect) list() {
-	//name = "📁 " + name
-	//name = "📄 " + name
-	var err error
-	fs.Dir, err = os.ReadDir(fs.Path)
+func (s *FilesystemSelect) list() {
+	dir, err := os.ReadDir(s.Path)
 	if err != nil {
 		// TODO:  Report the error
-		fs.container.Host.Close()
+		s.container.Host.Close()
 		return
+	}
+	if len(s.Extensions) > 0 {
+		s.Dir = make([]fs.DirEntry, 0, len(dir))
+		for i := range dir {
+			if dir[i].IsDir() || slices.Contains(s.Extensions, filepath.Ext(dir[i].Name())) {
+				s.Dir = append(s.Dir, dir[i])
+			}
+		}
+	} else {
+		s.Dir = dir
 	}
 }
