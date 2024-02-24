c1a0bc32ead744b68dd406e2bc730e633a6d9f32
Brent Farris <brentfarris@outlook.com>
2024-02-24 07:22:05 -0600

    Clean up editor cache code

--- src/editor/cache/editor_cache/projects.go
+++ src/editor/cache/editor_cache/projects.go
@@ -50,43 +50,27 @@ const (
 	projectListFile = "projects.txt"
 )
 
-func projectCacheFolder() (string, error) {
-	cache, err := os.UserCacheDir()
+func writeProjectList(list []string) error {
+	cache, err := cacheFolder()
 	if err != nil {
-		return "", err
+		return err
 	}
-	cache = filepath.Join(cache, CacheFolder)
-	if _, err := os.Stat(cache); os.IsNotExist(err) {
-		os.Mkdir(cache, os.ModePerm)
-	}
-	return cache, nil
-}
-
-func writeProjectList(cache string, list []string) error {
 	return filesystem.WriteTextFile(filepath.Join(cache, projectListFile), strings.Join(list, "\n"))
 }
 
 func AddProject(project string) error {
-	cache, err := projectCacheFolder()
-	if err != nil {
-		return err
-	}
 	list, err := ListProjects()
 	if err != nil {
 		return err
 	}
 	if !slices.Contains(list, project) {
 		list = append(list, project)
-		return writeProjectList(cache, list)
+		return writeProjectList(list)
 	}
 	return nil
 }
 
 func RemoveProject(project string) error {
-	cache, err := projectCacheFolder()
-	if err != nil {
-		return err
-	}
 	list, err := ListProjects()
 	if err != nil {
 		return err
@@ -99,14 +83,14 @@ func RemoveProject(project string) error {
 		}
 	}
 	if removed {
-		return writeProjectList(cache, list)
+		return writeProjectList(list)
 	}
 	return nil
 
 }
 
 func ListProjects() ([]string, error) {
-	cache, err := projectCacheFolder()
+	cache, err := cacheFolder()
 	if err != nil {
 		return []string{}, err
 	}
