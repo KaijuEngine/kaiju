34156ccbcc5aa7753ad96c04b68e1e41f79fac19
Brent Farris <brentfarris@outlook.com>
2024-03-04 07:08:31 -0600

    Copy headers

--- src/systems/console/errors.go
+++ src/systems/console/errors.go
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* vk_null64.go                                                               */
+/* errors.go                                                                  */
 /******************************************************************************/
 /*                           This file is part of:                            */
 /*                                KAIJU ENGINE                                */
@@ -35,63 +35,10 @@
 /* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                              */
 /******************************************************************************/
 
-// +build !386,!arm
+package console
 
-package vulkan
-
-import "unsafe"
+import "errors"
 
 var (
-	// NullHandle defines a platform-specfic NULL handle.
-	NullHandle unsafe.Pointer
-	// NullSemaphore defines a platform-specfic NULL Semaphore.
-	NullSemaphore Semaphore
-	// NullFence defines a platform-specfic NULL Fence.
-	NullFence Fence
-	// NullDeviceMemory defines a platform-specfic NULL DeviceMemory.
-	NullDeviceMemory DeviceMemory
-	// NullBuffer defines a platform-specfic NULL Buffer.
-	NullBuffer Buffer
-	// NullImage defines a platform-specfic NULL Image.
-	NullImage Image
-	// NullEvent defines a platform-specfic NULL Event.
-	NullEvent Event
-	// NullQueryPool defines a platform-specfic NULL QueryPool.
-	NullQueryPool QueryPool
-	// NullBufferView defines a platform-specfic NULL BufferView.
-	NullBufferView BufferView
-	// NullImageView defines a platform-specfic NULL ImageView.
-	NullImageView ImageView
-	// NullShaderModule defines a platform-specfic NULL ShaderModule.
-	NullShaderModule ShaderModule
-	// NullPipelineCache defines a platform-specfic NULL PipelineCache.
-	NullPipelineCache PipelineCache
-	// NullPipelineLayout defines a platform-specfic NULL PipelineLayout.
-	NullPipelineLayout PipelineLayout
-	// NullRenderPass defines a platform-specfic NULL RenderPass.
-	NullRenderPass RenderPass
-	// NullPipeline defines a platform-specfic NULL Pipeline.
-	NullPipeline Pipeline
-	// NullDescriptorSetLayout defines a platform-specfic NULL DescriptorSetLayout.
-	NullDescriptorSetLayout DescriptorSetLayout
-	// NullSampler defines a platform-specfic NULL Sampler.
-	NullSampler Sampler
-	// NullDescriptorPool defines a platform-specfic NULL DescriptorPool.
-	NullDescriptorPool DescriptorPool
-	// NullDescriptorSet defines a platform-specfic NULL DescriptorSet.
-	NullDescriptorSet DescriptorSet
-	// NullFramebuffer defines a platform-specfic NULL Framebuffer.
-	NullFramebuffer Framebuffer
-	// NullCommandPool defines a platform-specfic NULL CommandPool.
-	NullCommandPool CommandPool
-	// NullSurface defines a platform-specfic NULL Surface.
-	NullSurface Surface
-	// NullSwapchain defines a platform-specfic NULL Swapchain.
-	NullSwapchain Swapchain
-	// NullDisplay defines a platform-specfic NULL Display.
-	NullDisplay Display
-	// NullDisplayMode defines a platform-specfic NULL DisplayMode.
-	NullDisplayMode DisplayMode
-	// NullDebugReportCallback defines a platform-specfic NULL DebugReportCallback.
-	NullDebugReportCallback DebugReportCallback
+	ErrCommandNotFound = errors.New("the command with the given key does not exist")
 )
