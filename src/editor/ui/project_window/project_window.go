cbc68ad3ef3adacab826a97dac67207bc8df7932
Brent Farris <brentfarris@outlook.com>
2024-05-03 21:07:44 -0500

    Removing keyboard copy

--- src/editor/ui/project_window/project_window.go
+++ src/editor/ui/project_window/project_window.go
@@ -134,7 +134,6 @@ func (p *ProjectWindow) load() {
 			"newProject":    p.newProject,
 			"selectProject": p.selectProject,
 		})
-	p.container.Host.Window.CopyToClipboard("Test copy from code")
 }
 
 func New(templatePath string, cx, cy int) (*ProjectWindow, error) {
