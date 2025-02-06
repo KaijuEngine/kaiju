eefd887a6f1df096962378a1d00b6c5ea83c7e61
Brent Farris <brentfarris@outlook.com>
2025-02-06 16:16:51 -0600

    Adding duplicate, details, and focus right-click hierarchy options

--- src/editor/context_menu_sets.go
+++ src/editor/context_menu_sets.go
@@ -3,12 +3,22 @@ package editor
 import (
 	"kaiju/editor/ui/context_menu"
 	"kaiju/editor/viewport/tools/deleter"
+	"kaiju/editor/viewport/tools/duplicator"
 )
 
 func hierarchyContextMenuActions(ed *Editor) context_menu.ContextMenuSet {
 	entries := []context_menu.ContextMenuEntry{
 		{Id: "delete", Label: "Delete", OnClick: func() {
 			deleter.DeleteSelected(ed)
+		}},
+		{Id: "duplicate", Label: "Duplicate", OnClick: func() {
+			duplicator.DeleteSelected(ed)
+		}},
+		{Id: "details", Label: "Details", OnClick: func() {
+			ed.detailsWindow.Show()
+		}},
+		{Id: "focus", Label: "Focus", OnClick: func() {
+			ed.selection.Focus(ed.Host().Camera)
 		}},
 	}
 	return context_menu.NewSet(ed.contextMenu, entries)
