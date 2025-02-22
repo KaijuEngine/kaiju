84adb5b3094c3f0db1b729a878ef510d9e0f3eb9
Brent Farris <brentfarris@outlook.com>
2025-02-21 23:38:06 -0600

    Setting up all pipeline fields in UI

--- src/editor/ui/shader_designer_window/shader_designer_window.go
+++ src/editor/ui/shader_designer_window/shader_designer_window.go
@@ -8,12 +8,56 @@ import (
 )
 
 type ShaderDesignerData struct {
-	Vert     string
-	Frag     string
-	Geom     string
-	Tese     string
-	Tesc     string
-	Topology string
+	Vert                    string
+	Frag                    string
+	Geom                    string
+	Tese                    string
+	Tesc                    string
+	Topology                string
+	PrimitiveRestart        bool
+	DepthClampEnable        bool
+	RasterizerDiscardEnable bool
+	PolygonMode             string
+	CullMode                string
+	FrontFace               string
+	DepthBiasEnable         bool
+	DepthBiasConstantFactor string
+	DepthBiasClamp          string
+	DepthBiasSlopeFactor    string
+	LineWidth               string
+	RasterizationSamples    string
+	SampleShadingEnable     bool
+	MinSampleShading        string
+	AlphaToCoverageEnable   bool
+	AlphaToOneEnable        bool
+	LogicOpEnable           bool
+	LogicOp                 string
+	BlendConstants0         string
+	BlendConstants1         string
+	BlendConstants2         string
+	BlendConstants3         string
+	DepthTestEnable         bool
+	DepthWriteEnable        bool
+	DepthCompareOp          string
+	DepthBoundsTestEnable   bool
+	StencilTestEnable       bool
+	FrontFailOp             string
+	FrontPassOp             string
+	FrontDepthFailOp        string
+	FrontCompareOp          string
+	FrontCompareMask        string
+	FrontWriteMask          string
+	FrontReference          string
+	BackFailOp              string
+	BackPassOp              string
+	BackDepthFailOp         string
+	BackCompareOp           string
+	BackCompareMask         string
+	BackWriteMask           string
+	BackReference           string
+	MinDepthBounds          string
+	MaxDepthBounds          string
+	PatchControlPoints      string
 }
 
 func New() {
