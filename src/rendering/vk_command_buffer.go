4927d1917a3ffe1f7c64732c5278a386093f6e30
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2025-07-08 16:22:37 -0500

    Adding command buffer debug tracking

--- src/rendering/vk_command_buffer.go
+++ src/rendering/vk_command_buffer.go
@@ -114,6 +114,7 @@ func createCommandPoolBufferPair(vr *Vulkan, level vk.CommandBufferLevel) (Comma
 		SType: vulkan.StructureTypeFenceCreateInfo,
 	}
 	vulkan.CreateFence(vr.device, &fenceInfo, nil, &cr.fence)
+	vr.dbg.add(vk.TypeToUintPtr(cr.fence))
 	return cr, nil
 }
 
@@ -136,10 +137,11 @@ func (c *CommandRecorder) Begin() {
 func (c *CommandRecorder) Destroy(vr *Vulkan) {
 	buff := c.buffer
 	if buff != vk.NullCommandBuffer {
-	vk.FreeCommandBuffers(vr.device, c.pool, 1, &buff)
-	vk.DestroyCommandPool(vr.device, c.pool, nil)
-	vr.dbg.remove(vk.TypeToUintPtr(c.pool))
+		vk.FreeCommandBuffers(vr.device, c.pool, 1, &buff)
+		vk.DestroyCommandPool(vr.device, c.pool, nil)
+		vr.dbg.remove(vk.TypeToUintPtr(c.pool))
 		vulkan.DestroyFence(vr.device, c.fence, nil)
+		vr.dbg.remove(vk.TypeToUintPtr(c.fence))
 	}
 }
 
