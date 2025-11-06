b05fb274c51379d22b27a2fdbb4118d8cdd9341a
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2025-11-05 21:03:46 -0600

    Removing unused variable

--- src/engine/ui/label.go
+++ src/engine/ui/label.go
@@ -218,7 +218,6 @@ func (label *Label) renderText() {
 			maxWidth, ld.fgColor, ld.bgColor, ld.justify,
 			ld.baseline, label.entity.Transform.WorldScale(),
 			true, false, ld.fontFace, ld.lineHeight)
-		transparentDrawings := make([]rendering.Drawing, 0, len(ld.runeDrawings))
 		ld.runeShaderData = make([]*rendering.TextShaderData, len(ld.runeDrawings))
 		for i := range ld.runeDrawings {
 			rd := &ld.runeDrawings[i]
@@ -228,7 +227,6 @@ func (label *Label) renderText() {
 				transparent := ld.runeDrawings[i]
 				transparent.Material = host.FontCache().TransparentMaterial(
 					ld.runeDrawings[i].Material)
-				transparentDrawings = append(transparentDrawings, transparent)
 			}
 		}
 		for i := 0; i < len(ld.colorRanges); i++ {
