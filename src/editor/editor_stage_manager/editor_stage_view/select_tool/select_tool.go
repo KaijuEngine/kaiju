6ce0d27fefb9b756c87d58a448a03ae6fe9630d1
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2025-11-29 22:47:24 -0600

    #464 Fixing click being seen as box select

--- src/editor/editor_stage_manager/editor_stage_view/select_tool/select_tool.go
+++ src/editor/editor_stage_manager/editor_stage_view/select_tool/select_tool.go
@@ -37,8 +37,10 @@ func (s *SelectTool) Update() {
 		l.Scale(0.0001, 0.0001)
 		u.Clean()
 	} else if c.Released() {
-		s.handler.TryBoxSelect(s.box())
-		s.panel.Base().Hide()
+		if s.start.Distance(c.ScreenPosition()) > 5 {
+			s.handler.TryBoxSelect(s.box())
+			s.panel.Base().Hide()
+		}
 	} else if c.Held() {
 		box := s.box()
 		x, y := box.Left(), box.Top()
