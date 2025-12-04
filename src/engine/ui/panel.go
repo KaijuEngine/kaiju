0ef761e9811f838fda49512abffe9fea44a922d8
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2025-12-03 21:53:15 -0600

    Don't spam hide of panel scroll bars

--- src/engine/ui/panel.go
+++ src/engine/ui/panel.go
@@ -728,7 +728,7 @@ func (p *Panel) updateScrollBars() {
 	}
 	ps := p.layout.PixelSize()
 	panelW, panelH := ps.X(), ps.Y()
-	if pd.scrollBarX != nil {
+	if pd.scrollBarX != nil && pd.scrollBarX.Base().isActive() {
 		y := panelH - scrollBarWidth
 		pd.scrollBarX.layout.SetOffsetY(y)
 		maxX := pd.maxScroll.X()
@@ -758,7 +758,7 @@ func (p *Panel) updateScrollBars() {
 			pd.scrollBarX.Base().Hide()
 		}
 	}
-	if pd.scrollBarY != nil {
+	if pd.scrollBarY != nil && pd.scrollBarY.Base().isActive() {
 		x := panelW - scrollBarWidth
 		pd.scrollBarY.layout.SetOffsetX(x)
 		maxY := pd.maxScroll.Y()
