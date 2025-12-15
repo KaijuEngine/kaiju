3dbda2653fa72de03c4d93191df0d5140169e184
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2025-12-14 22:44:30 -0600

    #543 Improve entity data loading conversions

--- src/engine/entity_data_binding.go
+++ src/engine/entity_data_binding.go
@@ -40,6 +40,7 @@ import (
 	"errors"
 	"kaiju/build"
 	"kaiju/engine/runtime/encoding/gob"
+	"kaiju/matrix"
 	"log/slog"
 	"reflect"
 )
@@ -70,15 +71,38 @@ func ReflectEntityDataBindingValueFromJson(v any, f reflect.Value) {
 		fallthrough
 	case reflect.Slice:
 		elemType := f.Type().Elem()
-		if elemType.Kind() == reflect.Float32 {
-			ivs, ok := v.([]interface{})
-			if ok && len(ivs) == f.Len() {
+		if elemType.Kind() == reflect.Float32 || elemType.Kind() == reflect.Float64 {
+			if ivs, ok := v.([]interface{}); ok && len(ivs) == f.Len() {
 				for i := 0; i < f.Len(); i++ {
 					if num, ok := ivs[i].(float64); ok {
 						f.Index(i).SetFloat(num)
 					} else {
 						slog.Error("invalid float in array of floats", "index", i)
 					}
+				}
+			} else if ivs, ok := v.([]float32); ok && len(ivs) == f.Len() {
+				for i := 0; i < f.Len(); i++ {
+					f.Index(i).SetFloat(float64(ivs[i]))
+				}
+			} else if ivs, ok := v.([]float64); ok && len(ivs) == f.Len() {
+				for i := 0; i < f.Len(); i++ {
+					f.Index(i).SetFloat(ivs[i])
+				}
+			} else if vec, ok := v.(matrix.Vec2); ok {
+				for i := 0; i < len(vec); i++ {
+					f.Index(i).SetFloat(float64(vec[i]))
+				}
+			} else if vec, ok := v.(matrix.Vec3); ok {
+				for i := 0; i < len(vec); i++ {
+					f.Index(i).SetFloat(float64(vec[i]))
+				}
+			} else if vec, ok := v.(matrix.Vec4); ok {
+				for i := 0; i < len(vec); i++ {
+					f.Index(i).SetFloat(float64(vec[i]))
+				}
+			} else if vec, ok := v.(matrix.Color); ok {
+				for i := 0; i < len(vec); i++ {
+					f.Index(i).SetFloat(float64(vec[i]))
 				}
 			}
 		}
@@ -111,6 +135,8 @@ func ReflectEntityDataBindingValueFromJson(v any, f reflect.Value) {
 
 func reflectTo[F, T any](v any, f reflect.Value) {
 	if vv, ok := v.(F); ok {
+		f.Set(reflect.ValueOf(vv).Convert(reflect.TypeFor[T]()))
+	} else if vv, ok := v.(T); ok {
 		f.Set(reflect.ValueOf(vv).Convert(reflect.TypeFor[T]()))
 	}
 }
