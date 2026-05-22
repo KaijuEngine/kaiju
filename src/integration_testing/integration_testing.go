6a7ebeb7d8f2c058a74e94671f2259369f0a334f
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-05-21 22:35:13 -0500

    Updating integration test note

--- src/integration_testing/integration_testing.go
+++ src/integration_testing/integration_testing.go
@@ -19,7 +19,7 @@ func IntegrationTestGame(testName string) (*IntegrationGame, error) {
 	if test, ok := tests[testName]; ok {
 		return &IntegrationGame{test: test}, nil
 	}
-	return nil, fmt.Errorf("could not find test named %s", testName)
+	return nil, fmt.Errorf("could not find test named %s, perhaps you forgot to build the executable", testName)
 }
 
 func (IntegrationGame) PluginRegistry() []reflect.Type { return []reflect.Type{} }
