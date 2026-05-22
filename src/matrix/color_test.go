b49a56210b26dec72e14d42c40c5817b2b6a019f
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-05-22 18:46:40 -0500

    Updating copyright on code files

--- src/matrix/color_test.go
+++ src/matrix/color_test.go
@@ -1,37 +1,7 @@
 /******************************************************************************/
 /* color_test.go                                                              */
 /******************************************************************************/
-/*                            This file is part of                            */
-/*                                KAIJU ENGINE                                */
-/*                          https://kaijuengine.com/                          */
-/******************************************************************************/
-/* MIT License                                                                */
-/*                                                                            */
-/* Copyright (c) 2023-present Kaiju Engine authors (AUTHORS.md).              */
-/* Copyright (c) 2015-present Brent Farris.                                   */
-/*                                                                            */
-/* May all those that this source may reach be blessed by the LORD and find   */
-/* peace and joy in life.                                                     */
-/* Everyone who drinks of this water will be thirsty again; but whoever       */
-/* drinks of the water that I will give him shall never thirst; John 4:13-14  */
-/*                                                                            */
-/* Permission is hereby granted, free of charge, to any person obtaining a    */
-/* copy of this software and associated documentation files (the "Software"), */
-/* to deal in the Software without restriction, including without limitation  */
-/* the rights to use, copy, modify, merge, publish, distribute, sublicense,   */
-/* and/or sell copies of the Software, and to permit persons to whom the      */
-/* Software is furnished to do so, subject to the following conditions:       */
-/*                                                                            */
-/* The above copyright notice and this permission notice shall be included in */
-/* all copies or substantial portions of the Software.                        */
-/*                                                                            */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS    */
-/* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                 */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.     */
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY       */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT  */
-/* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE      */
-/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                              */
+/* MIT License, Copyright (c) 2015-present Brent Farris, (John 4:13-14)       */
 /******************************************************************************/
 
 package matrix
@@ -763,7 +733,7 @@ func TestColorZero(t *testing.T) {
 // Test that all named color constructors return valid colors (0 <= component <= 1 for non-alpha, 0 <= alpha <= 1)
 func TestNamedColorsValidRanges(t *testing.T) {
 	colors := []struct {
-		name string
+		name  string
 		color Color
 	}{
 		{"Red", ColorRed()},
@@ -1120,7 +1090,7 @@ func TestColorInvertedTwice(t *testing.T) {
 
 func BenchmarkNewColor(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = NewColor(Float(i%256) / 255.0, 0.5, 0.3, 1.0)
+		_ = NewColor(Float(i%256)/255.0, 0.5, 0.3, 1.0)
 	}
 }
 
