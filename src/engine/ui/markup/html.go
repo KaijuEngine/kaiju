7d0b017ab14ef5502f279ca1214b81c1f42b76a6
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-05-23 12:53:30 -0500

    Adding `kaiju-include` include to compose html files

--- src/engine/ui/markup/html.go
+++ src/engine/ui/markup/html.go
@@ -7,17 +7,31 @@
 package markup
 
 import (
+	"fmt"
+	"path"
+	"regexp"
+	"strings"
 	"weak"
 
+	"kaijuengine.com/engine/assets"
 	"kaijuengine.com/engine/ui"
 	"kaijuengine.com/engine/ui/markup/css"
 	"kaijuengine.com/engine/ui/markup/css/rules"
 	"kaijuengine.com/engine/ui/markup/document"
 )
 
+var (
+	htmlIncludeTagRE = regexp.MustCompile(`(?is)<kaiju-include\b([^>]*)>\s*</kaiju-include>|<kaiju-include\b([^>]*)/>`)
+	htmlIncludeSrcRE = regexp.MustCompile(`(?is)\bsrc\s*=\s*"([^"]+)"|\bsrc\s*=\s*'([^']+)'`)
+)
+
 func DocumentFromHTMLAsset(uiMan *ui.Manager, htmlPath string, withData any, funcMap map[string]func(*document.Element)) (*document.Document, error) {
 	host := uiMan.Host
 	m, err := host.AssetDatabase().ReadText(htmlPath)
+	if err != nil {
+		return nil, err
+	}
+	m, err = expandHTMLIncludes(host.AssetDatabase(), htmlPath, m, map[string]bool{})
 	if err != nil {
 		return nil, err
 	}
@@ -31,8 +45,75 @@ func DocumentFromHTMLAssetRooted(uiMan *ui.Manager, htmlPath string, withData an
 	if err != nil {
 		return nil, err
 	}
+	m, err = expandHTMLIncludes(host.AssetDatabase(), htmlPath, m, map[string]bool{})
+	if err != nil {
+		return nil, err
+	}
 	doc := DocumentFromHTMLString(uiMan, m, "", withData, funcMap, root)
 	return doc, nil
+}
+
+func expandHTMLIncludes(db assets.Database, ownerPath, html string, stack map[string]bool) (string, error) {
+	var firstErr error
+	expanded := htmlIncludeTagRE.ReplaceAllStringFunc(html, func(includeTag string) string {
+		if firstErr != nil {
+			return includeTag
+		}
+		matches := htmlIncludeTagRE.FindStringSubmatch(includeTag)
+		attrs := ""
+		if len(matches) > 1 {
+			attrs = strings.TrimSpace(matches[1])
+		}
+		if attrs == "" && len(matches) > 2 {
+			attrs = strings.TrimSpace(matches[2])
+		}
+		srcMatches := htmlIncludeSrcRE.FindStringSubmatch(attrs)
+		if len(srcMatches) == 0 {
+			firstErr = fmt.Errorf("kaiju-include in %s is missing a src attribute", ownerPath)
+			return includeTag
+		}
+		includePath := srcMatches[1]
+		if includePath == "" {
+			includePath = srcMatches[2]
+		}
+		includePath = resolveHTMLIncludePath(ownerPath, includePath)
+		if stack[includePath] {
+			firstErr = fmt.Errorf("kaiju-include cycle detected while reading %s", includePath)
+			return includeTag
+		}
+		stack[includePath] = true
+		includeHTML, err := db.ReadText(includePath)
+		if err != nil {
+			firstErr = fmt.Errorf("failed to read kaiju-include %s from %s: %w", includePath, ownerPath, err)
+			delete(stack, includePath)
+			return includeTag
+		}
+		includeHTML, err = expandHTMLIncludes(db, includePath, includeHTML, stack)
+		delete(stack, includePath)
+		if err != nil {
+			firstErr = err
+			return includeTag
+		}
+		return includeHTML
+	})
+	if firstErr != nil {
+		return "", firstErr
+	}
+	return expanded, nil
+}
+
+func resolveHTMLIncludePath(ownerPath, includePath string) string {
+	includePath = path.Clean(filepathToSlash(includePath))
+	if strings.HasPrefix(includePath, "/") ||
+		strings.HasPrefix(includePath, "editor/") ||
+		strings.Contains(includePath, "://") {
+		return strings.TrimPrefix(includePath, "/")
+	}
+	return path.Clean(path.Join(path.Dir(filepathToSlash(ownerPath)), includePath))
+}
+
+func filepathToSlash(p string) string {
+	return strings.ReplaceAll(p, "\\", "/")
 }
 
 func DocumentFromHTMLString(uiMan *ui.Manager, html, cssStr string, withData any, funcMap map[string]func(*document.Element), root *document.Element) *document.Document {
