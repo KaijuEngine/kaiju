e18773ed705cb28360e538a98e114bd52d6adffb
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-05-26 22:35:14 -0500

    Fixing fbx monkey integration test

--- src/integration_testing/integration_test_fbx_monkey.go
+++ src/integration_testing/integration_test_fbx_monkey.go
@@ -7,12 +7,13 @@
 package integration_testing
 
 import (
-	"log/slog"
 	"os"
 
 	"kaijuengine.com/engine"
-	"kaijuengine.com/framework"
+	"kaijuengine.com/engine/assets"
 	"kaijuengine.com/matrix"
+	"kaijuengine.com/registry/shader_data_registry"
+	"kaijuengine.com/rendering"
 	"kaijuengine.com/rendering/loaders"
 )
 
@@ -23,22 +24,41 @@ func init() {
 }
 
 func IntegrationTestFBXMonkey(host *engine.Host) {
-	host.PrimaryCamera().SetFarPlane(1000)
-	host.PrimaryCamera().SetPosition(matrix.Vec3Backward().Scale(320))
-	host.PrimaryCamera().SetLookAtWithUp(matrix.Vec3Zero(), matrix.Vec3Up())
+	position := matrix.Vec3Backward().Scale(3)
+	host.PrimaryCamera().SetPositionAndLookAt(position, matrix.Vec3Zero())
 	res, err := loaders.FBX("monkey.fbx", host.AssetDatabase())
 	if err != nil {
-		slog.Error("Failed to load FBX monkey", "error", err)
-		os.Exit(1)
+		panic(err)
 	}
-	drawings, err := framework.CreateDrawingsBasic(host, res)
+	mat, err := host.MaterialCache().Material(assets.MaterialDefinitionBasic)
 	if err != nil {
-		slog.Error("Failed to create FBX monkey drawings", "error", err)
-		os.Exit(1)
+		panic(err)
 	}
-	for _, drawing := range drawings.AllDrawings() {
-		host.Drawings.AddDrawing(drawing)
+	meshCache := host.MeshCache()
+	monkey := meshCache.Mesh("monkey", res.Meshes[0].Verts, res.Meshes[0].Indexes)
+	meshCache.AddMesh(monkey)
+	e := engine.NewEntity(host.WorkGroup())
+	sd := shader_data_registry.Create("cube")
+	sd.(*shader_data_registry.ShaderDataStandard).Color = matrix.ColorWhite()
+	first := ""
+	for _, v := range res.Meshes[0].Textures {
+		first = v
+		break
 	}
+	tex, err := host.TextureCache().Texture(first, rendering.TextureFilterLinear)
+	if err != nil {
+		tex, err = host.TextureCache().Texture(assets.TextureSquare, rendering.TextureFilterLinear)
+	}
+	if err != nil {
+		panic(err)
+	}
+	host.Drawings.AddDrawing(rendering.Drawing{
+		Material:   mat.CreateInstance([]*rendering.Texture{tex}),
+		Mesh:       monkey,
+		ShaderData: sd,
+		Transform:  &e.Transform,
+		ViewCuller: &host.Cameras.Primary,
+	})
 	host.RunAfterFrames(3, func() {
 		takeScreenshotToFile(host, fbxMonkeyScreenshotOutput)
 		os.Exit(0)
