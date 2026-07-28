fa2564da372839a2f4c55983ae0c184a0300ead4
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-07-27 21:53:09 -0500

    Fixing Steam build issues

--- src/platform/steam/steam_api.go
+++ src/platform/steam/steam_api.go
@@ -7,8 +7,8 @@
 package steam
 
 /*
+#cgo CPPFLAGS: -DBUILD_STEAM_API=1 -I${SRCDIR}/../../../publishing/steam_sdk/public
 #cgo CXXFLAGS: -std=c++11
-#cgo CFLAGS: -DBUILD_STEAM_API=1
 #cgo windows LDFLAGS: -L../../libs -lsteam_api64 -lstdc++
 #cgo steamdeck LDFLAGS: -L../../libs -lsteam_api -lstdc++ -Wl,-rpath=../../libs
 #cgo linux LDFLAGS: -L../../libs -lsteam_api -lstdc++ -Wl,-rpath=../../libs
